027ae0d6ea43b28a096289213d835e8ac4eba09d
Soroush Rabiei <soroush@ametisco.ir>
2017-09-15 00:25:46 +0430

    Update documentation
    Signed-off-by: Soroush Rabiei <soroush@ametisco.ir>

--- cl-math_8c.js
+++ cl-math_8c.js
@@ -1,6 +1,6 @@
 var cl_math_8c =
 [
     [ "clm_floor_div", "cl-math_8c.html#a53baf9bc4cf7d44c7c20b431f9194908", null ],
-    [ "clm_mod", "cl-math_8c.html#a9eb2dc0fb68b69e5d4bd4dd663e51263", null ],
-    [ "clm_pdiv", "cl-math_8c.html#acf94563bffe323e2182f8e1ae77be052", null ]
+    [ "clm_mod", "cl-math_8c.html#ad42f3ffb5429294a390bcf90d324912c", null ],
+    [ "clm_pdiv", "cl-math_8c.html#a8ac7c7f6d1bd74efe6ae794dec837dea", null ]
 ];
--- search/all_0.js
+++ search/all_0.js
@@ -14,7 +14,7 @@ var searchData=
   ['cl_2dsolar_2dhijri_2ec',['cl-solar-hijri.c',['../cl-solar-hijri_8c.html',1,'']]],
   ['cl_2dsolar_2dhijri_2eh',['cl-solar-hijri.h',['../cl-solar-hijri_8h.html',1,'']]],
   ['clm_5ffloor_5fdiv',['clm_floor_div',['../cl-math_8c.html#a53baf9bc4cf7d44c7c20b431f9194908',1,'clm_floor_div(int a, int b):&#160;cl-math.c'],['../cl-math_8h.html#a53baf9bc4cf7d44c7c20b431f9194908',1,'clm_floor_div(int a, int b):&#160;cl-math.c']]],
-  ['clm_5fmod',['clm_mod',['../cl-math_8c.html#a9eb2dc0fb68b69e5d4bd4dd663e51263',1,'clm_mod(int a, int b):&#160;cl-math.c'],['../cl-math_8h.html#a9eb2dc0fb68b69e5d4bd4dd663e51263',1,'clm_mod(int a, int b):&#160;cl-math.c']]],
-  ['clm_5fpdiv',['clm_pdiv',['../cl-math_8c.html#acf94563bffe323e2182f8e1ae77be052',1,'clm_pdiv(int d, int v):&#160;cl-math.c'],['../cl-math_8h.html#acf94563bffe323e2182f8e1ae77be052',1,'clm_pdiv(int d, int v):&#160;cl-math.c']]],
+  ['clm_5fmod',['clm_mod',['../cl-math_8c.html#ad42f3ffb5429294a390bcf90d324912c',1,'clm_mod(int x, int y):&#160;cl-math.c'],['../cl-math_8h.html#a9eb2dc0fb68b69e5d4bd4dd663e51263',1,'clm_mod(int a, int b):&#160;cl-math.c']]],
+  ['clm_5fpdiv',['clm_pdiv',['../cl-math_8c.html#a8ac7c7f6d1bd74efe6ae794dec837dea',1,'clm_pdiv(int y, int x):&#160;cl-math.c'],['../cl-math_8h.html#acf94563bffe323e2182f8e1ae77be052',1,'clm_pdiv(int d, int v):&#160;cl-math.c']]],
   ['cycle',['cycle',['../cl-solar-hijri_8c.html#aa23b3de21c5fa8dfe0b8e9d030000234',1,'cl-solar-hijri.c']]]
 ];
